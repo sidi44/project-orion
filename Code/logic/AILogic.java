1862e932cd928eb0aa44c762ae13b866119d4d84
sidi44 <simon_dicken@hotmail.com>
2015-05-17 10:21:11 +0100

    Tidied AILogic files.
    Changed MoveDir enum to Direction enum in AILogicRandom. Tidied
    documentation.

--- Code/logic/AILogic.java
+++ Code/logic/AILogic.java
@@ -3,22 +3,20 @@ package logic;
 /**
  * AILogic interface.
  * 
- * Classes which implement this interface should provide an implementation of
- * the calcNextMove() method which will use the current game state to 
- * calculate how the provided agent's will move. The agent's nextMove field 
- * should be set in the method.
+ * AILogic classes should calculate and set the next move of a given game Agent 
+ * based on the provided current GameState snapshot.
  * 
  * @author Simon Dicken
- * @version 2015-05-03
+ * @version 2015-05-16
  *
  */
 public interface AILogic {
 
 	/**
-	 * Uses to the provided state to calculate the agent's next move. The 
-	 * agent's nextMove field is set by the method.
+	 * Calculates and sets the agent's next move based on the provided GameState
+	 * snapshot.
 	 * 
-	 * @param agent - the Agent whose move should be calculated.
+	 * @param agent - the Agent whose move should be calculated (and set).
 	 * @param state - the current state of the game.
 	 */
 	void calcNextMove(Agent agent, GameState state);
--- Code/logic/AILogicRandom.java
+++ Code/logic/AILogicRandom.java
@@ -4,14 +4,15 @@ import java.util.Random;
 
 /**
  * AILogicRandom class.
+ * 
  * Implements the AILogic interface by simply setting the Agent's next move 
  * to be a random direction. 
  * 
  * @author Simon Dicken
- * @version 2015-05-03
+ * @version 2015-05-16
  */
 public class AILogicRandom implements AILogic {
-
+	
 	/**
 	 * Constructor for AILogicRandom.
 	 */
@@ -21,16 +22,15 @@ public class AILogicRandom implements AILogic {
 	
 	@Override
 	public void calcNextMove(Agent agent, GameState state) {
+
+		Random r = new Random();
 		
-		Random r = new Random(System.currentTimeMillis());
+		// Get all possible directions.
+		Direction[] allDir = Direction.values();
 		
-		// Get all possible moves.
-		MoveDir[] allMoves = MoveDir.values();
-		
-		// Get a random move and set the Agent's next move.
-		int dirNum = r.nextInt(allMoves.length);
-		Move m = new Move(allMoves[dirNum]);
-		agent.setNextMove(m);
+		// Get a random direction and set the Agent's next move.
+		int dirNum = r.nextInt(allDir.length);
+		agent.setNextMoveDirection(allDir[dirNum]);
 		
 	}
 
