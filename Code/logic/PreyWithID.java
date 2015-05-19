c1999a545e63203d339eed451354fad86483523c
MW777 <MW777@users.noreply.github.com>
2015-05-19 21:58:19 +0100

    Prey With ID

---
+++ Code/logic/PreyWithID.java
@@ -0,0 +1,11 @@
+package logic;
+
+import geometry.PointXY;
+
+public class Prey extends Agent {
+
+	public Prey(int id, PointXY position, boolean isPlayer) {
+		super(id, position, isPlayer);
+	}
+	
+}
