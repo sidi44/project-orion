6c2b45efd74e8cfa8f0269bc461b23af8337d385
MW777 <MW777@users.noreply.github.com>
2015-05-19 22:10:56 +0100

    Create Powerup.java

---
+++ Code/logic/Powerup.java
@@ -0,0 +1,14 @@
+package logic;
+
+public class Powerup {
+	private PowerType pType;
+	
+	public Powerup(PowerType pType) {
+		this.pType = pType;
+	}
+	
+	public PowerType getPType() {
+		return this.pType;
+	}
+	
+}
