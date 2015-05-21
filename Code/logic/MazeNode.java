7c9c0b130893b659bf4c10cd20ee1cdf298ebff2
MW777 <MW777@users.noreply.github.com>
2015-05-21 18:54:00 +0100

    With JavaDoc

--- Code/logic/MazeNode.java
+++ Code/logic/MazeNode.java
@@ -4,18 +4,41 @@ import geometry.PointXY;
 
 import java.util.Set;
 
+/**
+ * Represents a node which is used to build a maze.
+ * 
+ * @author Martin Wong
+ * @version 2015-05-19
+ */
 public class MazeNode {
 	
 	private Set<PointXY> neighbours;
 	
+	/**
+	 * Creates an instance of MazeNode.
+	 * 
+	 * @param neighbours (Set<PointXY>)
+	 */
 	public MazeNode(Set<PointXY> neighbours) {
 		this.neighbours = neighbours;
 	}
 	
+	/**
+	 * Gets the neighbours of this maze node.
+	 * 
+	 * @return neighbours (Set<PointXY>)
+	 */
 	public Set<PointXY> getNeighbours() {
 		return this.neighbours;
 	}
 	
+	/**
+	 * Add a new neighbour to this maze node and returns a boolean.
+	 * True is successful and False is unsuccessful.
+	 * 
+	 * @param position (PointXY)
+	 * @return success (boolean)
+	 */
 	public boolean addNeighbour(PointXY position) {
 		boolean success = false;
 		if (!this.neighbours.contains(position)) {
@@ -24,6 +47,13 @@ public class MazeNode {
 		return success;
 	}
 	
+	/**
+	 * Removes an existing neighbour from this maze node and returns a boolean.
+	 * True is successful and False is unsuccessful.
+	 * 
+	 * @param position (PointXY)
+	 * @return success (boolean)
+	 */
 	public boolean removeNeighbour(PointXY position) {
 		boolean success = false;
 		if (this.neighbours.contains(position)) {
@@ -32,6 +62,12 @@ public class MazeNode {
 		return success;
 	}
 	
+	/**
+	 * Checks whether a certain point on a graph is a neighbour of this maze node.
+	 * 
+	 * @param position (PointXY)
+	 * @return success (boolean)
+	 */
 	public boolean isNeighbour(PointXY position) {
 		return this.neighbours.contains(position);
 	}
