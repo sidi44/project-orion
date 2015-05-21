93932bf2a6e380ab1fd4dc006dfac5a2129f48b5
MW777 <MW777@users.noreply.github.com>
2015-05-21 18:56:07 +0100

    With JavaDoc

--- Code/logic/Prey.java
+++ Code/logic/Prey.java
@@ -2,6 +2,12 @@ package logic;
 
 import geometry.PointXY;
 
+/**
+ * Represents a predator agent.
+ * 
+ * @author Martin Wong
+ * @version 2015-05-19
+ */
 public class Prey extends Agent {
 
 	public Prey(PointXY position, boolean isPlayer) {
