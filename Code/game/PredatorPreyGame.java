ab831fb01fae89aa4c38226e47875748c3371506
sidi44 <simon_dicken@hotmail.com>
2015-07-16 21:26:54 +0100

    Updates to PredatorPreyGame for Renderer changes
    - Updated the defineAnimations() method in PredatorPreyGame for the new
    Renderer configuration classes.
    - Minor tweaks to PredatorPreyGame input parameters.

--- Code/game/PredatorPreyGame.java
+++ Code/game/PredatorPreyGame.java
@@ -12,9 +12,11 @@ import geometry.PolygonShape;
 import physics.PhysicsConfiguration;
 import physics.PhysicsProcessor;
 import physics.PhysicsProcessorBox2D;
-import render.AnimationConfiguration;
 import render.AnimationDefinition;
+import render.AnimationGroupDefinition;
 import render.Renderer;
+import render.RendererConfiguration;
+//import xml.ConfigurationXMLParser;
 import logic.Agent;
 import logic.AgentConfig;
 import logic.GameConfig;
@@ -58,8 +60,14 @@ public class PredatorPreyGame extends ApplicationAdapter {
 	private final static long nanoToSeconds = 1000000000;
 
 	@Override
-	public void create () {
+	public void create() {
 
+//		String filename = "xml\\Configuration.xml";
+//		ConfigurationXMLParser xmlParser = new ConfigurationXMLParser(filename);
+//		xmlParser.parseXML();
+//		PhysicsConfiguration test = xmlParser.getPhysicsConfig();
+//		RendererConfiguration test2 = xmlParser.getRendererConfig();
+		
 		startTime = System.nanoTime() / nanoToSeconds;
 		timeLimit = 200; // seconds.
 
@@ -73,9 +81,9 @@ public class PredatorPreyGame extends ApplicationAdapter {
 		MazeConfig mazeConfig = new MazeConfig(10, 50, 0.0, 0.8);
 		List<PointXY> vertices = new ArrayList<PointXY>();
 		vertices.add(new PointXY(0, 0));
-		vertices.add(new PointXY(0, 10));
-		vertices.add(new PointXY(10, 10));
-		vertices.add(new PointXY(10, 0));
+		vertices.add(new PointXY(0, 12));
+		vertices.add(new PointXY(12, 12));
+		vertices.add(new PointXY(12, 0));
 		List<PredatorPowerUp> predPowerUps = new ArrayList<PredatorPowerUp>();
 		PredatorPowerUp pow1 = 
 				new PredatorPowerUp(PredatorPowerType.SpeedUpPredator, 300);
@@ -89,7 +97,7 @@ public class PredatorPreyGame extends ApplicationAdapter {
 		gameLogic = new GameLogic(config);
 
 		PhysicsConfiguration physConfig = 
-				new PhysicsConfiguration(5f, 0.05f, 0.1f, 40f, 25f);
+				new PhysicsConfiguration(5f, 0.1f, 0.2f, 40f, 25f);
 
 		physProc = new PhysicsProcessorBox2D(world, gameLogic.getGameState(), 
 				physConfig);
@@ -115,19 +123,19 @@ public class PredatorPreyGame extends ApplicationAdapter {
 //		inputProcs.put(-1, inputProc);
 //		Gdx.input.setInputProcessor(inputProc);
 
-		camera = new OrthographicCamera(70, 70);
-		camera.position.x = 25;
-		camera.position.y = 25;
+		camera = new OrthographicCamera(75, 75);
+		camera.position.x = 35;
+		camera.position.y = 35;
 		camera.update();
-		renderer = new Renderer(true, false);
+		renderer = new Renderer(false, false);
 		
-		defineAnimations();
+		defineAnimations(config, physConfig);
 		
 		//shortestPath();
 	}
 
 	@Override
-	public void render () {
+	public void render() {
 		
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
@@ -171,11 +179,11 @@ public class PredatorPreyGame extends ApplicationAdapter {
 		switch (gameOver) {
 			case Pills:
 			case Time:
-				System.out.println("Prey won.");
+				//System.out.println("Prey won.");
 				break;
 
 			case Prey:
-				System.out.println("Predators won.");
+				//System.out.println("Predators won.");
 				break;
 
 			case No:
@@ -185,189 +193,176 @@ public class PredatorPreyGame extends ApplicationAdapter {
 
 	}
 
-	private void defineAnimations() {
+	private void defineAnimations(GameConfig gameConfig, 
+			PhysicsConfiguration physicsConfig) {
 
-		// Define the Predator animations
+		// Define the Predator Animation Group
+		AnimationGroupDefinition predatorDef = new AnimationGroupDefinition();
+		predatorDef.setAnimationGroupName("Predator");
+		predatorDef.setFilename("assets/predator3.png");
+		predatorDef.setColumns(11);
+		predatorDef.setRows(4);
+		
+		// Define the individual predator animations
 		AnimationDefinition def = new AnimationDefinition();
-		def.setAnimationGroupName("Predator");
-		def.setAnimationName(AnimationConfiguration.ANIMATION_DOWN_STOP);
-		def.setFilename("assets/predator2.png");
-		def.setColumns(11);
-		def.setRows(4);
+		def.setAnimationName(RendererConfiguration.ANIMATION_DOWN_STOP);
 		def.setStartFrame(1);
 		def.setEndFrame(1);
 		def.setFrameDuration(1 / 60f);
-
+		predatorDef.addAnimation(def);
+		
 		AnimationDefinition def2 = new AnimationDefinition();
-		def2.setAnimationGroupName("Predator");
-		def2.setAnimationName(AnimationConfiguration.ANIMATION_DOWN);
-		def2.setFilename("assets/predator2.png");
-		def2.setColumns(11);
-		def2.setRows(4);
+		def2.setAnimationName(RendererConfiguration.ANIMATION_DOWN);
 		def2.setStartFrame(2);
 		def2.setEndFrame(7);
 		def2.setFrameDuration(1 / 60f);
-
+		predatorDef.addAnimation(def2);
+		
 		AnimationDefinition def3 = new AnimationDefinition();
-		def3.setAnimationGroupName("Predator");
-		def3.setAnimationName(AnimationConfiguration.ANIMATION_UP_STOP);
-		def3.setFilename("assets/predator2.png");
-		def3.setColumns(11);
-		def3.setRows(4);
+		def3.setAnimationName(RendererConfiguration.ANIMATION_UP_STOP);
 		def3.setStartFrame(12);
 		def3.setEndFrame(12);
 		def3.setFrameDuration(1 / 60f);
-
+		predatorDef.addAnimation(def3);
+		
 		AnimationDefinition def4 = new AnimationDefinition();
-		def4.setAnimationGroupName("Predator");
-		def4.setAnimationName(AnimationConfiguration.ANIMATION_UP);
-		def4.setFilename("assets/predator2.png");
-		def4.setColumns(11);
-		def4.setRows(4);
+		def4.setAnimationName(RendererConfiguration.ANIMATION_UP);
 		def4.setStartFrame(13);
 		def4.setEndFrame(18);
 		def4.setFrameDuration(1 / 60f);
-
+		predatorDef.addAnimation(def4);
+		
 		AnimationDefinition def5 = new AnimationDefinition();
-		def5.setAnimationGroupName("Predator");
-		def5.setAnimationName(AnimationConfiguration.ANIMATION_LEFT_STOP);
-		def5.setFilename("assets/predator2.png");
-		def5.setColumns(11);
-		def5.setRows(4);
+		def5.setAnimationName(RendererConfiguration.ANIMATION_LEFT_STOP);
 		def5.setStartFrame(23);
 		def5.setEndFrame(23);
 		def5.setFrameDuration(1 / 60f);
-
+		predatorDef.addAnimation(def5);
+		
 		AnimationDefinition def6 = new AnimationDefinition();
-		def6.setAnimationGroupName("Predator");
-		def6.setAnimationName(AnimationConfiguration.ANIMATION_LEFT);
-		def6.setFilename("assets/predator2.png");
-		def6.setColumns(11);
-		def6.setRows(4);
+		def6.setAnimationName(RendererConfiguration.ANIMATION_LEFT);
 		def6.setStartFrame(24);
 		def6.setEndFrame(29);
 		def6.setFrameDuration(1 / 60f);
-
+		predatorDef.addAnimation(def6);
+		
 		AnimationDefinition def7 = new AnimationDefinition();
-		def7.setAnimationGroupName("Predator");
-		def7.setAnimationName(AnimationConfiguration.ANIMATION_RIGHT_STOP);
-		def7.setFilename("assets/predator2.png");
-		def7.setColumns(11);
-		def7.setRows(4);
+		def7.setAnimationName(RendererConfiguration.ANIMATION_RIGHT_STOP);
 		def7.setStartFrame(34);
 		def7.setEndFrame(34);
 		def7.setFrameDuration(1 / 60f);
-
+		predatorDef.addAnimation(def7);
+		
 		AnimationDefinition def8 = new AnimationDefinition();
-		def8.setAnimationGroupName("Predator");
-		def8.setAnimationName(AnimationConfiguration.ANIMATION_RIGHT);
-		def8.setFilename("assets/predator2.png");
-		def8.setColumns(11);
-		def8.setRows(4);
+		def8.setAnimationName(RendererConfiguration.ANIMATION_RIGHT);
 		def8.setStartFrame(35);
 		def8.setEndFrame(40);
 		def8.setFrameDuration(1 / 60f);
-
+		predatorDef.addAnimation(def8);
+		
+		// Define the Prey Animation Group
+		AnimationGroupDefinition preyDef = new AnimationGroupDefinition();
+		preyDef.setAnimationGroupName("Prey");
+		preyDef.setFilename("assets/prey.png");
+		preyDef.setColumns(11);
+		preyDef.setRows(4);
+		
 		// Define the Prey animations
 		AnimationDefinition def9 = new AnimationDefinition();
-		def9.setAnimationGroupName("Prey");
-		def9.setAnimationName(AnimationConfiguration.ANIMATION_DOWN_STOP);
-		def9.setFilename("assets/prey.png");
-		def9.setColumns(11);
-		def9.setRows(4);
+		def9.setAnimationName(RendererConfiguration.ANIMATION_DOWN_STOP);
 		def9.setStartFrame(1);
 		def9.setEndFrame(1);
 		def9.setFrameDuration(1 / 60f);
-
+		preyDef.addAnimation(def9);
+		
 		AnimationDefinition def10 = new AnimationDefinition();
-		def10.setAnimationGroupName("Prey");
-		def10.setAnimationName(AnimationConfiguration.ANIMATION_DOWN);
-		def10.setFilename("assets/prey.png");
-		def10.setColumns(11);
-		def10.setRows(4);
+		def10.setAnimationName(RendererConfiguration.ANIMATION_DOWN);
 		def10.setStartFrame(2);
 		def10.setEndFrame(7);
 		def10.setFrameDuration(1 / 60f);
+		preyDef.addAnimation(def10);
 
 		AnimationDefinition def11 = new AnimationDefinition();
-		def11.setAnimationGroupName("Prey");
-		def11.setAnimationName(AnimationConfiguration.ANIMATION_UP_STOP);
-		def11.setFilename("assets/prey.png");
-		def11.setColumns(11);
-		def11.setRows(4);
+		def11.setAnimationName(RendererConfiguration.ANIMATION_UP_STOP);
 		def11.setStartFrame(12);
 		def11.setEndFrame(12);
 		def11.setFrameDuration(1 / 60f);
-
+		preyDef.addAnimation(def11);
+		
 		AnimationDefinition def12 = new AnimationDefinition();
-		def12.setAnimationGroupName("Prey");
-		def12.setAnimationName(AnimationConfiguration.ANIMATION_UP);
-		def12.setFilename("assets/prey.png");
-		def12.setColumns(11);
-		def12.setRows(4);
+		def12.setAnimationName(RendererConfiguration.ANIMATION_UP);
 		def12.setStartFrame(13);
 		def12.setEndFrame(18);
 		def12.setFrameDuration(1 / 60f);
-
+		preyDef.addAnimation(def12);
+		
 		AnimationDefinition def13 = new AnimationDefinition();
-		def13.setAnimationGroupName("Prey");
-		def13.setAnimationName(AnimationConfiguration.ANIMATION_LEFT_STOP);
-		def13.setFilename("assets/prey.png");
-		def13.setColumns(11);
-		def13.setRows(4);
+		def13.setAnimationName(RendererConfiguration.ANIMATION_LEFT_STOP);
 		def13.setStartFrame(23);
 		def13.setEndFrame(23);
 		def13.setFrameDuration(1 / 60f);
-
+		preyDef.addAnimation(def13);
+		
 		AnimationDefinition def14 = new AnimationDefinition();
-		def14.setAnimationGroupName("Prey");
-		def14.setAnimationName(AnimationConfiguration.ANIMATION_LEFT);
-		def14.setFilename("assets/prey.png");
-		def14.setColumns(11);
-		def14.setRows(4);
+		def14.setAnimationName(RendererConfiguration.ANIMATION_LEFT);
 		def14.setStartFrame(24);
 		def14.setEndFrame(29);
 		def14.setFrameDuration(1 / 60f);
-
+		preyDef.addAnimation(def14);
+		
 		AnimationDefinition def15 = new AnimationDefinition();
-		def15.setAnimationGroupName("Prey");
-		def15.setAnimationName(AnimationConfiguration.ANIMATION_RIGHT_STOP);
-		def15.setFilename("assets/prey.png");
-		def15.setColumns(11);
-		def15.setRows(4);
+		def15.setAnimationName(RendererConfiguration.ANIMATION_RIGHT_STOP);
 		def15.setStartFrame(34);
 		def15.setEndFrame(34);
 		def15.setFrameDuration(1 / 60f);
-
+		preyDef.addAnimation(def15);
+		
 		AnimationDefinition def16 = new AnimationDefinition();
-		def16.setAnimationGroupName("Prey");
-		def16.setAnimationName(AnimationConfiguration.ANIMATION_RIGHT);
-		def16.setFilename("assets/prey.png");
-		def16.setColumns(11);
-		def16.setRows(4);
+		def16.setAnimationName(RendererConfiguration.ANIMATION_RIGHT);
 		def16.setStartFrame(35);
 		def16.setEndFrame(40);
 		def16.setFrameDuration(1 / 60f);	
-
-		AnimationConfiguration anConfig = new AnimationConfiguration();
-		anConfig.addAnimation(def);
-		anConfig.addAnimation(def2);
-		anConfig.addAnimation(def3);
-		anConfig.addAnimation(def4);
-		anConfig.addAnimation(def5);
-		anConfig.addAnimation(def6);
-		anConfig.addAnimation(def7);
-		anConfig.addAnimation(def8);
-		anConfig.addAnimation(def9);
-		anConfig.addAnimation(def10);
-		anConfig.addAnimation(def11);
-		anConfig.addAnimation(def12);
-		anConfig.addAnimation(def13);
-		anConfig.addAnimation(def14);
-		anConfig.addAnimation(def15);
-		anConfig.addAnimation(def16);
-
-		renderer.loadAnimations(anConfig);
+		preyDef.addAnimation(def16);
+		
+		
+		// Define the Pill Animation Group
+		AnimationGroupDefinition pillDef = new AnimationGroupDefinition();
+		pillDef.setAnimationGroupName("Pill");
+		pillDef.setFilename("assets/coin.png");
+		pillDef.setColumns(10);
+		pillDef.setRows(1);
+		
+		// Define the Prey animations
+		AnimationDefinition def17 = new AnimationDefinition();
+		def17.setAnimationName("");
+		def17.setStartFrame(1);
+		def17.setEndFrame(10);
+		def17.setFrameDuration(1.0f);
+		pillDef.addAnimation(def17);
+		
+		
+		// Define the configuration
+		RendererConfiguration rendererConfig = new RendererConfiguration();
+		rendererConfig.addAnimationGroup(predatorDef);
+		rendererConfig.addAnimationGroup(preyDef);
+		rendererConfig.addAnimationGroup(pillDef);
+		
+		rendererConfig.setAllowRotations(true);
+		rendererConfig.setBackgroundFilename("assets/vortex_0.png");
+		
+		PolygonShape dimensions = gameConfig.getDimensions();
+		int mazeWidth = dimensions.getMaxX() + 1 - dimensions.getMinX();
+		float width = mazeWidth * physicsConfig.getSquareSize();
+		int mazeHeight = dimensions.getMaxY() + 1 - dimensions.getMinY(); 
+		float height = mazeHeight * physicsConfig.getSquareSize();
+		Vector2 bottomLeft = new Vector2(0, 0);
+		Vector2 topRight = new Vector2(width, height);
+		rendererConfig.setBackgroundDimensions(bottomLeft, topRight);
+		
+		rendererConfig.setWallTextureFilename("assets/wall.png");
+		rendererConfig.setWallTextureScale(1.0f);
+		
+		renderer.loadTextures(rendererConfig);
 
 	}
 }
