794a4bed6b5a6d4d23526116f35b60994967a056
sidi44 <simon_dicken@hotmail.com>
2015-07-20 20:23:07 +0100

    Updated Animation and Group definition files

--- Code/render/AnimationDefinition.java
+++ Code/render/AnimationDefinition.java
@@ -1,11 +1,14 @@
 package render;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
 /**
  * This class defines a single animation in an animation group. 
  * It should be used by an XML parser to set the field values and then an 
  * instance of this class should then be passed on to AnimationConfiguration. 
  */
-
+@XmlRootElement(name = "AnimationDefinition")
 public class AnimationDefinition {
 
 	private String animationName;
@@ -29,18 +32,22 @@ public class AnimationDefinition {
 		return frameDuration;
 	}
 	
+	@XmlElement (name = "AnimationName")
 	public void setAnimationName(String animationName) {
 		this.animationName = animationName;
 	}
 	
+	@XmlElement (name = "StartFrame")
 	public void setStartFrame(int startFrame) {
 		this.startFrame = startFrame;
 	}
 	
+	@XmlElement (name = "EndFrame")
 	public void setEndFrame(int endFrame) {
 		this.endFrame = endFrame;
 	}
 	
+	@XmlElement (name = "FrameDuration")
 	public void setFrameDuration(float frameDuration) {
 		this.frameDuration = frameDuration;
 	}
--- Code/render/AnimationGroupDefinition.java
+++ Code/render/AnimationGroupDefinition.java
@@ -3,6 +3,10 @@ package render;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@XmlRootElement(name = "AnimationGroupDefinition")
 public class AnimationGroupDefinition {
 	
 	private String animationGroupName;
@@ -35,18 +39,28 @@ public class AnimationGroupDefinition {
 		return animationDefinitions;
 	}
 	
+	@XmlElement (name = "AnimationDefinition")
+	public void setAnimationDefinitions(
+			List<AnimationDefinition> animationDefinitions) {
+		this.animationDefinitions = animationDefinitions;
+	}
+	
+	@XmlElement (name = "AnimationGroupName")
 	public void setAnimationGroupName(String animationGroupName) {
 		this.animationGroupName = animationGroupName;
 	}
 	
+	@XmlElement (name = "Rows")
 	public void setRows(int rows) {
 		this.rows = rows;
 	}
 	
+	@XmlElement (name = "Columns")
 	public void setColumns(int columns) {
 		this.columns = columns;
 	}
 	
+	@XmlElement (name = "Filename")
 	public void setFilename(String filename) {
 		this.filename = filename;
 	}
