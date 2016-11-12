c221ebda263e7620e262a6cf5ac0e33f2eeda185
Pokerpunch <p.balasevicius@gmail.com>
2016-11-12 15:56:04 +0000

    Add pause screen

---
+++ Code/ui/PauseScreen.java
@@ -0,0 +1,110 @@
+package ui;
+
+import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.utils.Align;
+
+class PauseScreen extends MenuScreen {
+
+    // TODO remove main menu button from game screen since it's redundant -
+    // main menu option is already in pause screen dialog.
+
+    private static final float VERTICAL_BUTTON_PADDING = 10f;
+    private static final float HORIZONTAL_BUTTON_PADDING = 5f;
+
+    private static final String RESUME_BUTTON = "resume_button";
+    private static final String RESET_BUTTON = "reset_button";
+    private static final String HOME_BUTTON = "home_button";
+    private static final String SETTINGS_BUTTON = "settings_button";
+
+    private final GameScreen gameScreen;
+    private Dialog dialog;
+
+    public PauseScreen(ScreenManager manager) {
+        super(manager);
+
+         gameScreen = (GameScreen) getManager().getScreen(ScreenName.Game);
+    }
+
+
+    @Override
+    public void addActors() {
+
+        final int buttonSizeX = 75;
+        final int buttonSizeY = 75;
+
+        // Create a dialog
+        dialog = new Dialog("Game Paused", getSkin(), "dialog") {
+            @Override
+            public void result(Object obj) {
+
+                String buttonId = (String) obj;
+                ScreenName screenName;
+
+                if (RESUME_BUTTON.equals(buttonId)) {
+                    screenName = ScreenName.Game;
+                }
+                else if (RESET_BUTTON.equals(buttonId)) {
+                    getManager().getGame().resetGame();
+                    screenName = ScreenName.Game;
+                }
+                else if (SETTINGS_BUTTON.equals(buttonId)) {
+                    screenName = ScreenName.Settings;
+                }
+                else if (HOME_BUTTON.equals(buttonId)) {
+                    screenName = ScreenName.MainMenu;
+                }
+                else {
+                    throw new IllegalArgumentException("Invalid button ID: " + buttonId);
+                }
+
+                getManager().changeScreen(screenName);
+            }
+        };
+
+        // Pad it
+        dialog.getButtonTable().padTop(VERTICAL_BUTTON_PADDING);
+        dialog.getButtonTable().padBottom(VERTICAL_BUTTON_PADDING);
+        dialog.getButtonTable().padLeft(HORIZONTAL_BUTTON_PADDING);
+        dialog.getButtonTable().padRight(HORIZONTAL_BUTTON_PADDING);
+
+        // Align the title text
+        dialog.getTitleLabel().setAlignment(Align.center);
+
+        addDialogButton(RESUME_BUTTON, buttonSizeX, buttonSizeY);
+        addDialogButton(RESET_BUTTON, buttonSizeX, buttonSizeY);
+        addDialogButton(HOME_BUTTON, buttonSizeX, buttonSizeY);
+        addDialogButton(SETTINGS_BUTTON, buttonSizeX, buttonSizeY);
+
+        dialog.pack();
+        dialog.debug();
+    }
+
+
+    @Override
+    public void doRender(float delta) {
+
+        // TODO Stop camera movement and animations
+        gameScreen.doRender(0);
+
+        getManager().getGame().pause();
+    }
+
+
+    @Override
+    public void doShow() {
+        // This might be really inefficient, but prevents the dialog from disappearing
+        // after being accessed more than once
+        dialog.show(getStage());
+    }
+
+
+    private void addDialogButton(String styleName,
+                                 int buttonSizeX,
+                                 int buttonSizeY) {
+
+        final ImageButton dialogButton = new ImageButton(getSkin(), styleName);
+        dialog.getButtonTable().add(dialogButton).size(buttonSizeX, buttonSizeY);
+        dialog.setObject(dialogButton, styleName);
+    }
+}
