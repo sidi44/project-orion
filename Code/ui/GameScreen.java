bd923078892db3e5529819a3b2f5340fa711f8a7
Pokerpunch <p.balasevicius@gmail.com>
2016-12-04 17:12:59 +0000

    Add game over dialogs

--- Code/ui/GameScreen.java
+++ Code/ui/GameScreen.java
@@ -1,12 +1,5 @@
 package ui;
 
-import game.PredatorPreyGame;
-import input.UserInputProcessor;
-import logic.GameOver;
-import logic.GameState;
-import logic.Predator;
-import logic.powerup.PowerUp;
-
 import java.util.ArrayList;
 import java.util.List;
 
@@ -16,42 +9,49 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.input.GestureDetector;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
-import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 
+import game.PredatorPreyGame;
+import input.UserInputProcessor;
+import logic.GameOver;
+import logic.GameState;
+import logic.Predator;
+import logic.powerup.PowerUp;
+
 class GameScreen extends MenuScreen {
-	
+
 	private final CameraManager cameraManager;
 	private final UserInputProcessor inputProc;
-	
+
 	private Label scoreLabel;
 	private Label timeLabel;
 	private List<PowerUpButton> powerUpButtons;
-	
+
 	private final static int NUM_POWER_UP_BUTTONS = 5;
-	
+
 	public GameScreen(ScreenManager manager) {
 		super(manager);
-		
+
 		Camera gameCamera = new OrthographicCamera();
 		cameraManager = new CameraManager(gameCamera, manager.getGame());
-		
-		this.inputProc = new UserInputProcessor(cameraManager);
+
+		this.inputProc = new UserInputProcessor(cameraManager, manager.getGame());
 	}
-	
+
 	@Override
 	protected void addInputProcessor(InputMultiplexer multiplexer) {
 		multiplexer.addProcessor(inputProc);
 		multiplexer.addProcessor(new GestureDetector(inputProc));
 	}
-	
+
 	@Override
 	protected void addActors() {
-		
+
 		// Create pause button
-        Button pauseButton = new TextButton("Pause", getSkin());
+        Button pauseButton = new ImageButton(getSkin(), "pause_button");
         pauseButton.addListener(new ClickListener() {
             @Override
             public void clicked(InputEvent event, float x, float y) {
@@ -59,11 +59,15 @@ class GameScreen extends MenuScreen {
             }
         });
 
+        // Work out the size of the pause button based on the screen size
+        final float pauseButtonScale = 0.1f;
+        final int pauseButtonSize = getScaledSmallerScreenDimension(pauseButtonScale);
+
 		// Create the Score and Time remaining text fields
 		scoreLabel = new Label("Score: ", getSkin());
 		timeLabel = new Label("Time remaining: ", getSkin());
-		
-		// Create the Power Up buttons 
+
+		// Create the Power Up buttons
 		powerUpButtons = new ArrayList<PowerUpButton>();
 		for (int i = 0; i < NUM_POWER_UP_BUTTONS; ++i) {
 			final int index = i;
@@ -76,19 +80,19 @@ class GameScreen extends MenuScreen {
 				}
 			});
 		}
-		
-		// Create and setup our table. The UI has two 'panes'; the info pane 
-		// which contains the score, time remaining and menu button, and the 
+
+		// Create and setup our table. The UI has two 'panes'; the info pane
+		// which contains the score, time remaining and menu button, and the
 		// button pane which contains the power up buttons.
-		
+
 		// Pad our widgets a bit
 		float pad = 10f;
-		
+
 		// First create the info pane. The time remaining label is expanded in
-		// the X direction so the pane will fill the width of the screen. 
+		// the X direction so the pane will fill the width of the screen.
 		Table infoPane = new Table();
 		infoPane.add(timeLabel).pad(pad).left().expandX();
-		infoPane.add(pauseButton).pad(pad).right();
+		infoPane.add(pauseButton).size(pauseButtonSize).pad(pad).right();
 		infoPane.row();
 		infoPane.add(scoreLabel).pad(pad).left();
 
@@ -99,51 +103,51 @@ class GameScreen extends MenuScreen {
 			buttonPane.add(button).pad(pad).width(dim).height(dim);
 			buttonPane.row();
 		}
-		
-		// Now add the two panes to the main table. Make sure the info pane 
-		// fills the full width of the table. The button pane should expand to 
+
+		// Now add the two panes to the main table. Make sure the info pane
+		// fills the full width of the table. The button pane should expand to
 		// fill the remaining space in both directions, but the widgets it holds
 		// should be aligned to the left.
 		Table table = new Table();
 		table.add(infoPane).fillX();
 		table.row();
 		table.add(buttonPane).expand().left();
-		
+
 		// Add the table to our UI stage
 		table.setFillParent(true);
 		table.setDebug(true);
 		getStage().addActor(table);
 	}
-	
+
 	@Override
 	protected void doShow() {
 		// Set up the intial view
 		cameraManager.setInitialViewport();
-		
+
 		// Make sure the right number of power up buttons are visible
 		Predator predator = getPredator();
-		int numPowerUps = predator.getMaxPowerUp(); 
+		int numPowerUps = predator.getMaxPowerUp();
 		for (int i = 0; i < NUM_POWER_UP_BUTTONS; ++i) {
 			PowerUpButton button = powerUpButtons.get(i);
 			boolean visible = (i < numPowerUps);
 			button.setVisible(visible);
 		}
 	}
-	
+
 	@Override
 	protected void doRender(float delta) {
-		
+
 		PredatorPreyGame game = getManager().getGame();
 		GameState state = game.getGameLogic().getGameState();
-		
+
 		int score = state.getScore();
 		String scoreText = "Score : " + score;
 		scoreLabel.setText(scoreText);
-		
+
 		int timeRemaining = (int) state.getTimeRemaining();
 		String timeRemainingText = "Time remaining : " + timeRemaining + "s";
 		timeLabel.setText(timeRemainingText);
-		
+
 		Predator predator = getPredator();
 		int numPowerUps = predator.getMaxPowerUp();
 		for (int i = 0; i < NUM_POWER_UP_BUTTONS; ++i) {
@@ -152,7 +156,7 @@ class GameScreen extends MenuScreen {
 				powerUpButtons.get(i).setPowerUp(powerUp);
 			}
 		}
-		
+
 		Camera gameCamera = cameraManager.getCamera();
 		game.getRenderer().render(game.getWorld(), gameCamera.combined);
 		inputProc.processCameraInputs(gameCamera);
@@ -163,23 +167,23 @@ class GameScreen extends MenuScreen {
 		}
 		cameraManager.update();
 	}
-	
+
 	private void gameFinished(GameOver reason) {
-		
+
 		// Clear any stored user input
 		inputProc.reset();
-		
+
 		// Grab the game from the screen manager
 		PredatorPreyGame game = getManager().getGame();
-		
+
 		// Tell the game its finished and why
 		game.gameOver(reason);
 	}
-	
+
 	/**
-	 * Convenience method to get the game's predator. 
+	 * Convenience method to get the game's predator.
 	 * This assumes there is exactly one predator.
-	 * 
+	 *
 	 * @return the game's predator.
 	 */
 	private Predator getPredator() {
--- Code/ui/PauseScreen.java
+++ Code/ui/PauseScreen.java
@@ -1,20 +1,36 @@
 package ui;
 
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
 import com.badlogic.gdx.scenes.scene2d.ui.Dialog;
 import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
 import com.badlogic.gdx.utils.Align;
 
+import game.GameType;
+
 class PauseScreen extends MenuScreen {
 
+    // TODO
+    // Use some DialogBuilder or this will get messy
+
+    // Button constants
     private static final float VERTICAL_BUTTON_PADDING = 10f;
     private static final float HORIZONTAL_BUTTON_PADDING = 5f;
-
+    private static final float BUTTON_SCALE = 0.15f;
     private static final String RESUME_BUTTON = "resume_button";
     private static final String RESET_BUTTON = "reset_button";
     private static final String HOME_BUTTON = "home_button";
     private static final String SETTINGS_BUTTON = "settings_button";
+    private static final String NEXT_BUTTON = "next_button";
+    private static final String EXIT_BUTTON = "exit_button";
 
-    private Dialog dialog;
+    private int buttonWidth;
+    private int buttonHeight;
+
+    // Dialog key constants
+    private Dialog gamePausedDialog;
+    private Dialog gameWonDialog;
+    private Dialog gameLostDialog;
+
 
     public PauseScreen(ScreenManager manager) {
         super(manager);
@@ -23,73 +39,220 @@ class PauseScreen extends MenuScreen {
     @Override
     public void addActors() {
 
-        final int buttonSizeX = 75;
-        final int buttonSizeY = 75;
+        buttonWidth = getScaledSmallerScreenDimension(BUTTON_SCALE);
+        buttonHeight = getScaledSmallerScreenDimension(BUTTON_SCALE);
 
-        // Create a dialog
-        dialog = new Dialog("Game Paused", getSkin(), "dialog") {
+        gamePausedDialog = createGamePausedDialog();
+        gameWonDialog = createGameWonDialog();
+        gameLostDialog = createGameLostDialog();
+    }
+
+
+    @Override
+    public void doShow() {
+        super.doShow();
+
+        switch (getManager().getGame().getGameOverReason()) {
+
+          case No:
+              gamePausedDialog.show(getStage());
+              break;
+
+          case Prey:
+              gameWonDialog.show(getStage());
+              break;
+
+          default:
+              gameLostDialog.show(getStage());
+              break;
+        }
+    }
+
+
+    @Override
+    public void doRender(float delta) {
+        super.doRender(delta);
+        getManager().getScreen(ScreenName.Game).render(0);
+    }
+
+
+    /**
+     * Helper method that creates and adds a button with the specified style
+     * and dimensions to the dialog.
+     *
+     * @param dialog The dialog.
+     * @param styleName The style name for the dialog button.
+     * @param buttonWidth The button width.
+     * @param buttonHeight The button height.
+     */
+    private Button addDialogButton(Dialog dialog,
+                                 String styleName,
+                                 int buttonWidth,
+                                 int buttonHeight) {
+
+        final ImageButton dialogButton = new ImageButton(getSkin(), styleName);
+        dialog.getButtonTable().add(dialogButton).size(buttonWidth, buttonHeight);
+        dialog.setObject(dialogButton, styleName);
+
+        return dialogButton;
+    }
+
+
+    private Dialog createGamePausedDialog() {
+
+        Dialog gamePausedDialog = new Dialog("Game Paused", getSkin(), "dialog_transparent_background") {
             @Override
             public void result(Object obj) {
-
-                String buttonId = (String) obj;
-                ScreenName screenName;
-
-                if (RESUME_BUTTON.equals(buttonId)) {
-                    screenName = ScreenName.Game;
-                }
-                else if (RESET_BUTTON.equals(buttonId)) {
-                    getManager().getGame().resetGame();
-                    screenName = ScreenName.Game;
-                }
-                else if (SETTINGS_BUTTON.equals(buttonId)) {
-                	SettingsScreen settingsScreen = 
-                			(SettingsScreen) getManager().getScreen(ScreenName.Settings);
-                	settingsScreen.setPreviousScreen(ScreenName.Pause);
-                    screenName = ScreenName.Settings;
-                }
-                else if (HOME_BUTTON.equals(buttonId)) {
-                    screenName = ScreenName.MainMenu;
-                }
-                else {
-                    throw new IllegalArgumentException("Invalid button ID: " + buttonId);
-                }
-
-                getManager().changeScreen(screenName);
+                changeScreenOnButtonPressed((String) obj);
             }
         };
 
         // Pad it
-        dialog.getButtonTable().padTop(VERTICAL_BUTTON_PADDING);
-        dialog.getButtonTable().padBottom(VERTICAL_BUTTON_PADDING);
-        dialog.getButtonTable().padLeft(HORIZONTAL_BUTTON_PADDING);
-        dialog.getButtonTable().padRight(HORIZONTAL_BUTTON_PADDING);
+        gamePausedDialog.getButtonTable().padTop(VERTICAL_BUTTON_PADDING);
+        gamePausedDialog.getButtonTable().padBottom(VERTICAL_BUTTON_PADDING);
+        gamePausedDialog.getButtonTable().padLeft(HORIZONTAL_BUTTON_PADDING);
+        gamePausedDialog.getButtonTable().padRight(HORIZONTAL_BUTTON_PADDING);
 
         // Align the title text
-        dialog.getTitleLabel().setAlignment(Align.center);
+        gamePausedDialog.getTitleLabel().setAlignment(Align.center);
 
-        addDialogButton(RESUME_BUTTON, buttonSizeX, buttonSizeY);
-        addDialogButton(RESET_BUTTON, buttonSizeX, buttonSizeY);
-        addDialogButton(HOME_BUTTON, buttonSizeX, buttonSizeY);
-        addDialogButton(SETTINGS_BUTTON, buttonSizeX, buttonSizeY);
+        addDialogButton(gamePausedDialog, RESUME_BUTTON, buttonWidth, buttonHeight);
+        addDialogButton(gamePausedDialog, RESET_BUTTON, buttonWidth, buttonHeight);
+        addDialogButton(gamePausedDialog, SETTINGS_BUTTON, buttonWidth, buttonHeight);
+        addDialogButton(gamePausedDialog, EXIT_BUTTON, buttonWidth, buttonHeight);
 
-        dialog.pack();
-        dialog.debug();
-    }
+        gamePausedDialog.pack();
+        gamePausedDialog.debug();
 
-    @Override
-    public void doShow() {
-        // This might be really inefficient, but prevents the dialog from disappearing
-        // after being accessed more than once
-        dialog.show(getStage());
+        return gamePausedDialog;
     }
 
 
-    private void addDialogButton(String styleName,
-                                 int buttonSizeX,
-                                 int buttonSizeY) {
+    private Dialog createGameLostDialog() {
 
-        final ImageButton dialogButton = new ImageButton(getSkin(), styleName);
-        dialog.getButtonTable().add(dialogButton).size(buttonSizeX, buttonSizeY);
-        dialog.setObject(dialogButton, styleName);
+        Dialog gameLostDialog = new Dialog("Game lost", getSkin(), "dialog_transparent_background") {
+
+            @Override
+            public void result(Object obj) {
+                changeScreenOnButtonPressed((String) obj);
+            }
+        };
+
+        // Pad it
+        gameLostDialog.getButtonTable().padTop(VERTICAL_BUTTON_PADDING);
+        gameLostDialog.getButtonTable().padBottom(VERTICAL_BUTTON_PADDING);
+        gameLostDialog.getButtonTable().padLeft(HORIZONTAL_BUTTON_PADDING);
+        gameLostDialog.getButtonTable().padRight(HORIZONTAL_BUTTON_PADDING);
+
+        // Align the title text
+        gameLostDialog.getTitleLabel().setAlignment(Align.center);
+
+        // Set the dialog text
+        gameLostDialog.text("You lost!");
+
+        addDialogButton(gameLostDialog, RESET_BUTTON, buttonWidth, buttonHeight);
+        addDialogButton(gameLostDialog, EXIT_BUTTON, buttonWidth, buttonHeight);
+
+        gameLostDialog.pack();
+        gameLostDialog.debug();
+
+        return gameLostDialog;
+    }
+
+
+    private Dialog createGameWonDialog() {
+
+        Dialog gameWonDialog = new Dialog("Game won", getSkin(), "dialog_transparent_background") {
+
+            @Override
+            public void result(Object obj) {
+                changeScreenOnButtonPressed((String) obj);
+            }
+        };
+
+        // Pad it
+        gameWonDialog.getButtonTable().padTop(VERTICAL_BUTTON_PADDING);
+        gameWonDialog.getButtonTable().padBottom(VERTICAL_BUTTON_PADDING);
+        gameWonDialog.getButtonTable().padLeft(HORIZONTAL_BUTTON_PADDING);
+        gameWonDialog.getButtonTable().padRight(HORIZONTAL_BUTTON_PADDING);
+
+        // Align the title text
+        gameWonDialog.getTitleLabel().setAlignment(Align.center);
+
+        // Set the dialog text
+        gameWonDialog.text("You won!");
+
+        StarDisplayPanel starPanel = new StarDisplayPanel(getSkin());
+
+        starPanel.setCompleteSilver(); // TODO call PPGame.getStarCount();
+        gameWonDialog.getContentTable().row();
+        gameWonDialog.getContentTable().add(starPanel);
+
+        addDialogButton(gameWonDialog, NEXT_BUTTON, buttonWidth, buttonHeight);
+        addDialogButton(gameWonDialog, RESET_BUTTON, buttonWidth, buttonHeight);
+        addDialogButton(gameWonDialog, EXIT_BUTTON, buttonWidth, buttonHeight);
+
+        gameWonDialog.pack();
+        gameWonDialog.debug();
+
+        return gameWonDialog;
+    }
+
+
+    private ScreenName getExitScreenName() {
+
+        GameType gameType = getManager().getGame().getGameType();
+        ScreenName screenName;
+
+        switch (gameType) {
+
+        case Levels:
+            screenName = ScreenName.Levels;
+            break;
+
+        case Sandbox:
+            screenName = ScreenName.Sandbox;
+            break;
+
+        default:
+            throw new IllegalStateException("Invalid game type: " + gameType);
+        }
+
+        return screenName;
+    }
+
+
+    private void changeScreenOnButtonPressed(String buttonId) {
+
+        ScreenName screenName;
+
+        if (RESUME_BUTTON.equals(buttonId)) {
+            screenName = ScreenName.Game;
+        }
+        else if (RESET_BUTTON.equals(buttonId)) {
+            getManager().getGame().resetGame();
+            screenName = ScreenName.Game;
+        }
+        else if (SETTINGS_BUTTON.equals(buttonId)) {
+            SettingsScreen settingsScreen =
+                    (SettingsScreen) getManager().getScreen(ScreenName.Settings);
+            settingsScreen.setPreviousScreen(ScreenName.Pause);
+            screenName = ScreenName.Settings;
+        }
+        else if (HOME_BUTTON.equals(buttonId)) {
+            screenName = ScreenName.MainMenu;
+        }
+        else if (NEXT_BUTTON.equals(buttonId)) {
+            // TODO
+            throw new IllegalStateException("Not implemented");
+        }
+        else if (EXIT_BUTTON.equals(buttonId)) {
+            screenName = getExitScreenName();
+        }
+        else {
+            throw new IllegalArgumentException("Invalid button ID: " + buttonId);
+        }
+
+        getManager().changeScreen(screenName);
     }
 }
